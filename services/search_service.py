e260b1eded4992228954621d1bb9021399fed5a5
Aristotelis <aristotelis.30fyllidis@gmail.com>
2021-06-22 16:31:09 +0100

    Genus and classification in find around me command

--- services/search_service.py
+++ services/search_service.py
@@ -1,34 +1,73 @@
 import math
 import re
+from typing import List
 
 from data import redis_session
 
 
-async def create_sets_based_on_classification(agent_address: str, classification: str) -> None:
+async def create_sets_based_on_classification(
+    agent_address: str, classification: str
+) -> None:
     client = await redis_session.create_async_session()
-    classification_list = classification.split('.')
+    classification_list = classification.split(".")
     table = ""
     for item in classification_list:
-        table += "."+item
+        table += "." + item
         await client.sadd(table[1:], agent_address)
 
 
-async def create_sets_based_on_genus(agent_address:str, genus: str) -> None:
+async def create_sets_based_on_genus(agent_address: str, genus: str) -> None:
     client = await redis_session.create_async_session()
     await client.sadd(genus, agent_address)
 
 
-def special_match(strg, search=re.compile(r'[^a-z.]').search):
+def special_match(strg, search=re.compile(r"[^a-z.]").search):
     return not bool(search(strg))
 
 
-async def find_around_me(radius: float, latitude: float, longitude: float, genus: str = None, classification: str = None):
+async def find_around_me(
+    radius: float,
+    latitude: float,
+    longitude: float,
+    genus: str = None,
+    classification: str = None,
+) -> List[str]:
     client = await redis_session.create_async_session()
-    entries = await client.smembers('v_agents')
+    members = await find_members(genus, classification)
     agents = []
-    for address in entries:
+    for address in members:
         agent = await client.hgetall(address)
-        calculate_distance = abs(math.sqrt((latitude - float(agent.get('_latitude'))) ** 2 + (longitude - float(agent.get('_longitude'))) ** 2) - radius)
+        calculate_distance = abs(
+            math.sqrt(
+                (latitude - float(agent.get("_latitude"))) ** 2
+                + (longitude - float(agent.get("_longitude"))) ** 2
+            )
+            - radius
+        )
         if calculate_distance < radius:
             agents.append(address)
     return agents
+
+
+async def find_members(genus: str, classification: str):
+    client = await redis_session.create_async_session()
+    members = None
+    if genus is not None:
+        members = await client.smembers("genus")
+    if classification is not None:
+        redis_tables = await redis_session.get_table_name_by_classification(
+            classification
+        )
+        if members is None:
+            for key in redis_tables:
+                members += await client.smembers(key)
+        elif members is not None:
+            for key in redis_tables:
+                returned_members = await client.smembers(key)
+                for item in members:
+                    if item not in returned_members:
+                        members.remove(item)
+    if genus is None and classification is None:
+        members = await client.smembers("v_agents")
+
+    return members
